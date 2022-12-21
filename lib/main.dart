60900152fa12ab3b18beb8e43a6fee2a57db3a4f
kaya-hasan <hasankaya3546@hotmail.com>
2022-12-21 15:23:47 +0300

    proje bitti

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_tasarim/gonderiKarti.dart';
 import 'package:instagram_tasarim/profilSayfasi.dart';
@@ -57,8 +56,19 @@ class _AnaSayfaState extends State<AnaSayfa> {
               color: Colors.pink[300],
               size: 32.0,
             ),
-            onPressed: () {},
-          )
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Column(
+                      children: <Widget>[
+                        duyuru("Ayşe seni takip etti", "3dk önce"),
+                        duyuru("Fatma seni takip etti", "1 saat önce"),
+                      ],
+                    );
+                  }); // duyurular
+            },
+          ),
         ],
       ),
       body: ListView(
@@ -78,22 +88,52 @@ class _AnaSayfaState extends State<AnaSayfa> {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                profilKartiOlustur("Ali",
-                    "https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"),
-                profilKartiOlustur("Ahmet",
-                    "https://cdn.pixabay.com/photo/2021/06/15/16/11/man-6339003_960_720.jpg"),
-                profilKartiOlustur("Mehmet",
-                    "https://cdn.pixabay.com/photo/2016/11/23/00/33/man-1851469_960_720.jpg"),
-                profilKartiOlustur("Linda",
-                    "https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_960_720.jpg"),
-                profilKartiOlustur("Georgina",
-                    "https://cdn.pixabay.com/photo/2020/06/26/03/54/young-5341577_960_720.jpg"),
-                profilKartiOlustur("Becky",
-                    "https://cdn.pixabay.com/photo/2020/05/11/11/23/woman-5157666_960_720.jpg"),
-                profilKartiOlustur("Yusuf",
-                    "https://instagram.fadb5-1.fna.fbcdn.net/v/t51.2885-15/280156097_145407964673069_1595558480382984499_n.webp?stp=dst-jpg_e35_s320x320&_nc_ht=instagram.fadb5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=Oabnr5qYkVUAX8UCVeP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDrDhIh2mgm0xGNKdyvu3o5B5q7CVWzNkoAe9PbMCVVng&oe=63A67BD4&_nc_sid=8fd12b"),
-                profilKartiOlustur("Samet",
-                    "https://instagram.fadb5-1.fna.fbcdn.net/v/t51.2885-15/292936804_783509806117481_3499548620888248506_n.jpg?stp=c0.180.1440.1440a_dst-jpg_e35_s320x320&_nc_ht=instagram.fadb5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=B-VHbUk2UxoAX-98u30&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfD5eumxUBVoMvNMIyaxuL70Vp0bhd1hS2Oi2Ym-xdVhpA&oe=63A62A07&_nc_sid=8fd12b"),
+                profilKartiOlustur(
+                  "Ali",
+                  "https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg",
+                  "Ali Taş",
+                  "https://fotolifeakademi.com/uploads/2020/04/manzara-fotografi-cekmek-724x394.webp",
+                ),
+                profilKartiOlustur(
+                  "Ahmet",
+                  "https://cdn.pixabay.com/photo/2021/06/15/16/11/man-6339003_960_720.jpg",
+                  "Ahmet Tutum",
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6M6uhX92vkIonk8UnXxVpHKNuipYWVB1XrQ&usqp=CAU",
+                ),
+                profilKartiOlustur(
+                  "Mehmet",
+                  "https://cdn.pixabay.com/photo/2016/11/23/00/33/man-1851469_960_720.jpg",
+                  "Mehmet Şimşek",
+                  "https://lavinya.net/wp-content/uploads/2022/10/5b9895-manzara-landscape-scaled.jpeg",
+                ),
+                profilKartiOlustur(
+                  "Linda",
+                  "https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_960_720.jpg",
+                  "Linda Eczacıbaşı",
+                  "https://images.unsplash.com/photo-1591448764624-d7973a442bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFuemFyYXxlbnwwfHwwfHw%3D&w=1000&q=80",
+                ),
+                profilKartiOlustur(
+                  "Georgina",
+                  "https://cdn.pixabay.com/photo/2020/06/26/03/54/young-5341577_960_720.jpg",
+                  "Georgina Ronaldo",
+                  "https://images.odamax.com/img/1024x768/odamax/image/upload/ZmlsZU5hbWU9MjAyMTA1MTNfMTE1NzMw_20210619151117.jpg",
+                ),
+                profilKartiOlustur(
+                  "Becky",
+                  "https://cdn.pixabay.com/photo/2020/05/11/11/23/woman-5157666_960_720.jpg",
+                  "Becky Fring",
+                  "https://gunceloku.com/uploads/manzara-fotografi-duzenleme-61432376283a7.jpg",
+                ),
+                profilKartiOlustur(
+                    "Yusuf",
+                    "https://instagram.fadb5-1.fna.fbcdn.net/v/t51.2885-15/280156097_145407964673069_1595558480382984499_n.webp?stp=dst-jpg_e35_s320x320&_nc_ht=instagram.fadb5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=Oabnr5qYkVUAX8UCVeP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDrDhIh2mgm0xGNKdyvu3o5B5q7CVWzNkoAe9PbMCVVng&oe=63A67BD4&_nc_sid=8fd12b",
+                    "Yusuf Öz",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKAFqsc2tlyhVftjpCqjonp6njosC6-0xgqw&usqp=CAU"),
+                profilKartiOlustur(
+                    "Samet",
+                    "https://instagram.fadb5-1.fna.fbcdn.net/v/t51.2885-15/292936804_783509806117481_3499548620888248506_n.jpg?stp=c0.180.1440.1440a_dst-jpg_e35_s320x320&_nc_ht=instagram.fadb5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=B-VHbUk2UxoAX-98u30&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfD5eumxUBVoMvNMIyaxuL70Vp0bhd1hS2Oi2Ym-xdVhpA&oe=63A62A07&_nc_sid=8fd12b",
+                    "Samet Aydın",
+                    "https://img.piri.net/mnresize/840/-/resim/imagecrop/2022/10/28/03/37/resized_1213c-6297f917suriyehalep2.jpg"),
               ],
             ),
           ),
@@ -138,15 +178,44 @@ class _AnaSayfaState extends State<AnaSayfa> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.pink[600],
+        child: Icon(Icons.add_a_photo_sharp, color: Colors.white,),
+        ),
+    );
+
+  }
+
+  Padding duyuru(String mesaj, String gecenSure) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            mesaj,
+            style: TextStyle(fontSize: 15.0),
+          ),
+          Text(gecenSure),
+        ],
+      ),
     );
   }
 
-  Widget profilKartiOlustur(String kullaniciAdi, String resimLinki) {
+  Widget profilKartiOlustur(String kullaniciAdi, String resimLinki,
+      String adSoyad, String kapakResimLinki) {
     return Material(
       child: InkWell(
-        onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) => ProfilSayfasi()));
+        onTap: () async {
+          Future<dynamic> donenVeri = Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => ProfilSayfasi(
+                    profilResimLinki: resimLinki,
+                    adSoyad: adSoyad,
+                    kapakResimLinki: kapakResimLinki,
+                    kullaniciAdi: kullaniciAdi,
+                  )));
+          print(donenVeri);
         },
         child: Padding(
           padding: const EdgeInsets.only(left: 8.0, right: 8.0),
@@ -155,16 +224,19 @@ class _AnaSayfaState extends State<AnaSayfa> {
               Stack(
                 alignment: Alignment.topRight,
                 children: <Widget>[
-                  Container(
-                    width: 70,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(width: 2.0, color: Colors.grey),
-                      borderRadius: BorderRadius.circular(35.0),
-                      image: DecorationImage(
-                        image: NetworkImage(resimLinki),
-                        fit: BoxFit.cover,
+                  Hero(
+                    tag: kullaniciAdi,
+                    child: Container(
+                      width: 70,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(width: 2.0, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(35.0),
+                        image: DecorationImage(
+                          image: NetworkImage(resimLinki),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
--- lib/profilSayfasi.dart
+++ lib/profilSayfasi.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'gonderiKarti.dart';
+
 class ProfilSayfasi extends StatelessWidget {
-  const ProfilSayfasi({super.key});
+  final String adSoyad;
+  final String kullaniciAdi;
+  final String kapakResimLinki;
+  final String profilResimLinki;
+
+  const ProfilSayfasi({super.key, required this.adSoyad, required this.kullaniciAdi, required this.kapakResimLinki, required this.profilResimLinki});
+  
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +21,38 @@ class ProfilSayfasi extends StatelessWidget {
             children: <Widget>[
               Container(
                 height: 230.0,
-                color: Colors.yellow,
+                //color: Colors.yellow,
               ),
               Container(
                 height: 180.0,
-                color: Colors.green,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        kapakResimLinki),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
               Positioned(
+
                 left: 20.0,
                 bottom: 0.0,
-                child: Container(
-                  width: 120.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlue,
-                    borderRadius: BorderRadius.circular(60.0),
-                    border: Border.all(width: 2.0, color: Colors.white),
+                child: Hero(
+                  tag: kullaniciAdi,
+                  child: Container(
+                    width: 120.0,
+                    height: 120.0,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(60.0),
+                      border: Border.all(width: 2.0, color: Colors.white),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            profilResimLinki),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -39,7 +63,7 @@ class ProfilSayfasi extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Text(
-                      "Ahmet Tutum",
+                      adSoyad,
                       style: TextStyle(
                         fontSize: 18.0,
                         fontWeight: FontWeight.bold,
@@ -47,7 +71,7 @@ class ProfilSayfasi extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      "ahmettutum",
+                      kullaniciAdi,
                       style: TextStyle(
                         fontSize: 15.0,
                         fontWeight: FontWeight.bold,
@@ -57,10 +81,104 @@ class ProfilSayfasi extends StatelessWidget {
                   ],
                 ),
               ),
+              Positioned(
+                top: 130.0,
+                right: 15.0,
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14.0),
+                    color: Colors.grey[200],
+                    border: Border.all(
+                      width: 2.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_circle,
+                        size: 19.0,
+                      ),
+                      SizedBox(
+                        width: 2.1,
+                      ),
+                      Text("Takip et",
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+              ),
             ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            height: 75.0,
+            color: Colors.grey.withOpacity(0.1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                sayac("Takipçi", "1M"),
+                sayac("Takip Edilen", "0"),
+                sayac("Gönderi Sayısı", "80")
+              ],
+            ),
+          ),
+          GonderiKarti(
+            adSoyad: "Ahmet Tutum",
+            gecenSure: "15 dakika önce",
+            aciklama: "Florida müthiş bir yer!",
+            profilResimLinki:
+                "https://cdn.pixabay.com/photo/2021/06/15/16/11/man-6339003_960_720.jpg",
+            gonderiResimLinki:
+                "https://media.istockphoto.com/id/802893644/tr/foto%C4%9Fraf/downtown-miami-florida-havadan-g%C3%B6r%C3%BCn%C3%BCm%C3%BC.jpg?s=1024x1024&w=is&k=20&c=zhcj9srY1eE5yYVmiK1ATEmlSEQmQkyuNCq37lT15YI=",
           ),
         ],
       ),
+    );
+  }
+
+  Column sayac(String baslik, String sayi) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          sayi,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        SizedBox(
+          height: 1.0,
+        ),
+        Text(
+          baslik,
+          style: TextStyle(
+            fontSize: 15.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[600],
+          ),
+        ),
+      ],
     );
   }
 }
